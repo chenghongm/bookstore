0b2160291b42f118674c4180434a939b69b26807
chenghong meng <mch8418@gmail.com>
2020-04-08 21:51:12 -0400

    turn list display into grid and add cart

--- bookstore/src/main/java/com/womentech/bookstore/repository/BookRepository.java
+++ bookstore/src/main/java/com/womentech/bookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.womentech.bookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.womentech.bookstore.entity.Book;
 
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
