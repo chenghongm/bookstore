af65faf155c60518858036097f1d6305d2ccbe6a
chenghong meng <mch8418@gmail.com>
2020-04-10 22:39:49 -0400

    define routes in angular

---
+++ angular-bookstore/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookService } from './serives/book.service';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+   {path: 'books', component: BookListComponent},
+   {path: 'category/:id', component: BookListComponent},
+   {path: '', redirectTo:'books', pathMatch: 'full'},
+   {path: '**', component: PageNotFoundComponent},
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [
+    BookService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
