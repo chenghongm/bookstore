96b93e3b171959dbe6fd5e9911ea795a739c4713
chenghong meng <mch8418@gmail.com>
2020-04-16 17:36:18 -0400

    fix the cross-origin issues in branch of search-by-keyword

--- bookstore/src/main/java/com/womentech/bookstore/entity/BookCategory.java
+++ bookstore/src/main/java/com/womentech/bookstore/entity/BookCategory.java
@@ -17,11 +17,38 @@ import lombok.ToString;
 
 @Entity
 @Table(name="tbl_category")
-@Setter
-@Getter
-@ToString
+
 public class BookCategory {
 	
+	@Override
+	public String toString() {
+		return "BookCategory [id=" + id + ", categoryName=" + categoryName + ", book=" + book + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public Set<Book> getBook() {
+		return book;
+	}
+
+	public void setBook(Set<Book> book) {
+		this.book = book;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
