6b2d2a6d492a65067e43514b4284234b48522ff4
chenghong meng <mch8418@gmail.com>
2020-04-16 17:34:00 -0400

    add angular to master

---
+++ angular-bookstore/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookService } from './serives/book.service';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { BookCategoryComponent } from './components/book-category/book-category.component';
+import { SearchComponent } from './components/search/search.component';
+
+const routes: Routes = [
+   {path: 'books', component: BookListComponent},
+   {path: 'search/:keyword', component: BookListComponent},
+   {path: 'category/:id', component: BookListComponent},
+   {path: '', redirectTo:'/books', pathMatch: 'full'},
+   {path: '**', component: PageNotFoundComponent},
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent,
+    PageNotFoundComponent,
+    BookCategoryComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [
+    BookService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/common/book';
+import { BookService } from 'src/app/serives/book.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-grid.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+  books: Book[];
+  currentCategoryId: number;
+  searchMode: boolean;
+  counter: number
+  
+   constructor( private _bookSerive: BookService, private _activedRoute: ActivatedRoute) { }
+
+  ngOnInit(){
+    this._activedRoute.paramMap.subscribe(()=>{
+    
+     this.listBooks();
+    })
+  }
+  
+  
+    listBooks(){
+      
+       this.searchMode = this._activedRoute.snapshot.paramMap.has('keyword');
+     
+      if(this.searchMode){
+       
+        //do your search work
+        this.handleSearchBooks();
+      }else{
+        //display 
+        this.handleListBooks();
+      }
+
+    }
+    handleListBooks(){
+      const hasCategoryId: boolean = this._activedRoute.snapshot.paramMap.has('id');
+    if (hasCategoryId){
+      this.currentCategoryId = +this._activedRoute.snapshot.paramMap.get('id');
+    }else{
+      this.currentCategoryId = 1;
+    }
+
+    this._bookSerive.getBooks(this.currentCategoryId).subscribe(
+      data => this.books = data
+    )
+    }
+
+    handleSearchBooks(){
+      const keyword: string = this._activedRoute.snapshot.paramMap.get('keyword');
+
+      this._bookSerive.searchBooks(keyword).subscribe(
+        data => this.books = data
+      )
+    }
+
+}
